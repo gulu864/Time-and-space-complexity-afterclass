37cd11f88a7a34942570096cf93be1db45123169
gulu864 <suva.kanta4u@gmail.com>
2025-04-25 13:19:03 +0530

    Add files via upload

---
+++ Afterclassproject.py
@@ -0,0 +1,15 @@
+var1 = int(input("Enter a number: "))
+var2 = int(input("Enter a number: "))
+
+n = var1
+n1 = var2
+
+def multiply(n):
+
+    n = n * n1
+    print(n)
+
+print("\n", multiply(n))
+
+print("N iterations: ", multiply(n))
+print("1 Iterations: ", multiply(n))
